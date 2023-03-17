09674e4210ade60df8862790101e36b1690a7eaa
Gronorf <mauricio.mgu@gmail.com>
2023-03-17 09:53:43 -0300

    .

--- src/sprint/Usuario.java
+++ src/sprint/Usuario.java
@@ -1,4 +1,13 @@
 package sprint;
 
+import java.time.LocalDate;
+
 public class Usuario {
+    String nombre;
+    LocalDate fechaNacimiento;
+    int run;
+
+    public int mostrarEdad() {
+        return 2023 - fechaNacimiento.getYear();
+    }
 }
